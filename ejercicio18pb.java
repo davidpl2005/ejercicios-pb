7df8c9b1f162a375aabff5f62efb11943d19ade7
isac pacheco lopez <isac.pachecolopez@unicolombo.edu.co>
2024-02-25 11:10:53 -0500

    ejercicio numero 18

---
+++ ejercicio18pb.java
@@ -0,0 +1,11 @@
+//18.Calcular y escribir el cuadrado de 125.
+public class ejercicio18pb {
+ public static void main(String[] args) {
+
+int numero, cuadrado_num;
+numero = 125;
+cuadrado_num = 125 * 125;
+
+System.out.println(" El duadrado del numero: " + numero + " es: " + cuadrado_num);    
+    }
+}
